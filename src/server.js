59c74297a1d8de822c310a678b0d118de090b1fc
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-03-30 19:08:42 -0300

    change server config

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./app/routes');
 const { errorHandler } = require('./app/middlewares');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 // ROTAS
